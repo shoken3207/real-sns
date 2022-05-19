b1c865660da33cd021ad713b55bc14f8e199cdd6
shoken3207 <shoken4797@gmail.com>
2022-05-19 20:03:52 +0900

    rightbar完成

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -5,7 +5,58 @@ function Rightbar() {
   return (
     <div className="rightbar">
       <div className="rightbarWrapper">
-        <div className="eventContainer"></div>
+        <div className="eventContainer">
+          <img src="assets/star.png" alt="" className="starImg" />
+          <span className="eventText">
+            <b>フォロワー限定</b>イベント開催中！
+          </span>
+        </div>
+        <img src="assets/event.jpeg" className="eventImg" alt="" />
+        <h4 className="rightbarTitle">オンラインの友達</h4>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                className="rightbarProfileImg"
+                src="assets/person/1.jpeg"
+                alt=""
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Shin Code</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                className="rightbarProfileImg"
+                src="assets/person/2.jpeg"
+                alt=""
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Tanaka</span>
+          </li>
+        </ul>
+
+        <p className="promotionTitle">プロモーション広告</p>
+        <img
+          src="assets/promotion/promotion1.jpeg"
+          alt=""
+          className="rightbarPromotionImg"
+        />
+        <p className="promotionName">ショッピング</p>
+        <img
+          src="assets/promotion/promotion2.jpeg"
+          alt=""
+          className="rightbarPromotionImg"
+        />
+        <p className="promotionName">カーショップ</p>
+        <img
+          src="assets/promotion/promotion3.jpeg"
+          alt=""
+          className="rightbarPromotionImg"
+        />
+        <p className="promotionName">ShinCode株式会社</p>
       </div>
     </div>
   );
